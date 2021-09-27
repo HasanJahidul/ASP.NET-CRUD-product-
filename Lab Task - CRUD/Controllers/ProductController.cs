75b4bcb4ab5fbe20ee0ace610429242154d632f1
HasanJahidul <joyaiub1@gmail.com>
2021-09-27 14:20:47 +0600

    Product List Completed

---
+++ Lab Task - CRUD/Controllers/ProductController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using Lab_Task___CRUD.Models;
+using Lab_Task___CRUD.Models.Entity;
+
+namespace Lab_Task___CRUD.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult List()
+        {
+            Database db = new Database();
+            var products = db.Products.GetAll();
+            return View(products);
+        }
+        [HttpGet]
+        
+        public ActionResult Create()
+        {
+            Product p = new Product();
+            return View(p);
+        }
+        [HttpPost]
+        public ActionResult Create( Product p)
+        {
+
+            if (ModelState.IsValid)
+            {
+                Database db = new Database();
+                db.Products.Create(p);
+                return RedirectToAction("List");
+            }
+            return View(p);
+        }
+        public ActionResult Edit(int id)
+        {
+            Database db = new Database();
+            var s = db.Products.Get(id);
+            return View(s);
+        }
+        [HttpGet]
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ Lab Task - CRUD/Models/Tables/Products.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lab_Task___CRUD.Models.Entity;
+using System.Data.SqlClient;
+
+namespace Lab_Task___CRUD.Models.Tables
+{
+    public class Products
+    {
+        SqlConnection conn;
+        public Products(SqlConnection conn)
+        {
+            this.conn = conn;
+        }
+        public void Create(Product p)
+        {
+            string query = String.Format("Insert into Products values ('{name}','{price}','{quantity}','{description}')", p.Name, p.Price,
+                p.Description);
+            SqlCommand cmd = new SqlCommand(query, conn);
+            conn.Open();
+            int r = cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+        
+        public List<Product> GetAll()
+        {
+            string query = "select * from Products";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<Product> products = new List<Product>();
+            while (reader.Read())
+            {
+                Product p = new Product()
+                {
+                    Id = reader.GetInt32(reader.GetOrdinal("id")),
+                    Name = reader.GetString(reader.GetOrdinal("name")),
+                    Price = reader.GetInt32(reader.GetOrdinal("price")),
+                    Quantity = reader.GetString(reader.GetOrdinal("quantity")),
+                    Description  = reader.GetString(reader.GetOrdinal("description")),
+
+                };
+                products.Add(p);
+            }
+            conn.Close();
+            return products;
+        }
+        public Product Get(int id)
+        {
+            conn.Open();
+            string query = String.Format("Select * from Students where Id={0}", id);
+            SqlCommand cmd = new SqlCommand(query, conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            Product p = null;
+            while (reader.Read())
+            {
+                p = new Product()
+                {
+                    Id = reader.GetInt32(reader.GetOrdinal("id")),
+                    Name = reader.GetString(reader.GetOrdinal("name")),
+                    Price = reader.GetInt32(reader.GetOrdinal("price")),
+                    Quantity = reader.GetString(reader.GetOrdinal("quantity")),
+                    Description = reader.GetString(reader.GetOrdinal("description")),
+                };
+            }
+            conn.Close();
+            return p;
+        }
+
+    }
+}
